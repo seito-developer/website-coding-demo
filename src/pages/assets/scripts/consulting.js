8d3bdc013f6afdc5f2a6a837a5aae6e45b2bb1c8
Seito Horiguchi <seito.horiguchi@gmail.com>
2024-04-23 14:09:25 +0900

    removed

--- src/pages/assets/scripts/consulting.js
+++
@@ -1,6 +0,0 @@
-import { Tab } from "./modules/tab";
-
-new Tab({
-  trigger: '#js-tab',
-  content: "#js-content"
-});
--- src/pages/assets/scripts/siid.js
+++
@@ -1,7 +0,0 @@
-import { hero } from "./modules/hero";
-import { stream } from "./modules/stream";
-import modal from "./modules/modal";
-
-hero();
-stream();
-modal();
