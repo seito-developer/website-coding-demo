999fa42174415fb71a37b05c2e3fd43f000c11f9
Seito Horiguchi <seito.horiguchi@gmail.com>
2024-04-23 18:14:38 +0900

    finished

--- vite.config.js
+++ vite.config.js
@@ -5,32 +5,11 @@ import { mainData } from "./src/data/mainData";
 import VitePluginBrowserSync from "vite-plugin-browser-sync";
 import { globSync } from "glob";
 import path from "path";
-import { JSDOM } from 'jsdom';
 
 const ip = Object.values(networkInterfaces())
   .flat()
   .find((i) => i.family === "IPv4" && !i.internal)?.address;
 const root = "src/pages/";
-
-const moveScript = () => {
-  return {
-    name: "move-scripts",
-    transformIndexHtml(html) {
-      const dom = new JSDOM(html);
-      const document = dom.window.document;
-
-      // <head>内の<script>タグを検索し、条件に合致するものを移動
-      const scripts = Array.from(document.head.querySelectorAll('script[src*="assets"][src*=".js"]'));
-      scripts.forEach(script => {
-        // <script>タグを<body>の終わりに移動
-        document.body.appendChild(script);
-      });
-
-      // 新しいHTMLをシリアライズして返す
-      return dom.serialize();
-    }
-  };
-};
 
 export default defineConfig({
   root: root,
@@ -52,7 +31,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    moveScript(),
     VitePluginBrowserSync(),
     ViteEjsPlugin(mainData, {
       ejs: {
