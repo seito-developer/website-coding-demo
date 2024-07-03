dd93b53a9217440265d566b5a35f5213bf8840bb
seito-developer <seito.horiguchi@gmail.com>
2024-07-03 19:03:18 +0900

    improve: simpleに

--- src/data/mainData.js
+++ src/data/mainData.js
@@ -1,5 +1,20 @@
-import { common } from './common';
-
 export const mainData = {
-  common: common,
+  common: {
+    siteName: "SiiD Webサイト",
+    domain: "bug-fix.org",
+    title: {
+      index: "オンラインのプログラミングスクール「SiiD」 - BugFix LLC",
+      contact: "お問い合わせ | オンラインのプログラミングスクール「SiiD」",
+      privacyPolicy:
+        "プライバシーポリシー | オンラインのプログラミングスクール「SiiD」",
+    },
+    disc: {
+      index:
+        "オンラインのプログラミングスクール「SiiD」では、ゼミ形式でプログラミングやソフトウェア開発の手法をレクチャーします。カジュアルに始められるBasicコースと、ストイックに取り組むCareerコースがあります。",
+      contact:
+        "お問い合わせフォームです。弊社BugFixへはこちらからお気軽にお問い合わせください。",
+      privacyPolicy:
+        "オンラインのプログラミングスクール「SiiD」では、ゼミ形式でプログラミングやソフトウェア開発の手法をレクチャーします。カジュアルに始められるBasicコースと、ストイックに取り組むCareerコースがあります。",
+    },
+  },
 };
