8d7e431599ce0ca2028a8d4bbe0a337c3539cca5
seito-developer <seito.horiguchi@gmail.com>
2024-07-22 22:12:50 +0900

    fixed paths

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,26 @@
-import { defineConfig } from "vite";
-import { ViteEjsPlugin } from "vite-plugin-ejs";
-import { mainData } from "./src/data/mainData";
+import { defineConfig } from 'vite';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
+import { mainData } from './src/data/mainData';
+import { globSync } from 'glob';
+import path from 'path';
 
-const root = "src/pages/";
+const root = 'src/pages/';
+
+const inputFiles = globSync(`${root}/**/*.html`).reduce((entries, file) => {
+  const fileNameWithoutExt = path.relative(root, file).replace(path.extname(file), '');
+  entries[fileNameWithoutExt] = file;
+  return entries;
+}, {});
 
 export default defineConfig({
   root: root,
-  publicDir: "../../public",
+  publicDir: '../../public',
   build: {
-    outDir: "../../dist",
+    outDir: '../../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: inputFiles,
+    },
   },
   plugins: [
     ViteEjsPlugin(mainData, {
