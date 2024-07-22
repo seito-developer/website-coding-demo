9191e34b7872244007caa0b82713439720748106
seito-developer <seito.horiguchi@gmail.com>
2024-07-22 22:13:18 +0900

    add something

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import { mainData } from "./src/data/mainData";
+import { globSync } from "glob";
+import path from "path";
 
 const root = "src/pages/";
+
+const inputFiles = globSync(`${root}/**/*.html`).reduce((entries, file) => {
+  const fileNameWithoutExt = path
+    .relative(root, file)
+    .replace(path.extname(file), "");
+  entries[fileNameWithoutExt] = file;
+  return entries;
+}, {});
 
 export default defineConfig({
   root: root,
@@ -10,6 +20,9 @@ export default defineConfig({
   build: {
     outDir: "../../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: inputFiles,
+    },
   },
   plugins: [
     ViteEjsPlugin(mainData, {
